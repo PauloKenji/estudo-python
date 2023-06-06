f8d43f0e4c9158cfd3f5e99635091b9d33cff6ea
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-06 14:08:00 -0300

    aula 02 - keywords e identificadores

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,37 @@
+""" Aula 02 - Keywords e identificadores """
+
+# Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+# devem iniciar com letras ou _
+# nao pode ter espaço em branco
+# nao pode ter caracteres especiais !, @, #, $, %, ^, &, *, (, ), -, +, =,
+# {, }, [, ], |, \, :, ;, ", ', <, >, ,, ?, /
+
+nome = "Maria"
+idade = 20
+Nome = "João"
+nome_completo = "Maria Silva"
+
+# Boas práticas ( clean code )
+
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# constantes
+
+PI = 3.14
+
+idade = 18
+MAIORIDADE = 18
+
+if idade >= MAIORIDADE:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
