ec70b21408e78eea161163df992e3f0f7affcf89
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-06 13:51:27 -0300

    commit inicial

---
+++ src/01-intro/aula01.py
@@ -0,0 +1,12 @@
+"""aula01"""
+
+print('olá mundo')
+
+
+def somar(numero_1, numero_2):
+    """Soma dois números e retorna o resultado"""
+    return numero_1 + numero_2
+
+
+SOMA = somar(2, 2)
+print(SOMA)
