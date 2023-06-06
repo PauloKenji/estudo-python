b3bf6dac2e171be8eb0836604c64d2c59c3a5552
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-06 14:08:23 -0300

    aula 03 - comentários

---
+++ src/01-intro/aula03.py
@@ -0,0 +1,15 @@
+""" aula 03 - Comentários """
+
+# Comentário de uma linha
+
+'''
+Comentário de
+múltiplas linhas
+'''
+
+numero_1 = 10
+numero_2 = 20
+numero_3 = 30
+
+print(numero_1, numero_2)
+print(numero_3)
