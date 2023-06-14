34677b3e90b79fa5bfa34e5de154e58823395441
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-14 14:40:02 -0300

    04: exercicio 02

---
+++ src/04-funcoes/exercícios/ex02.py
@@ -0,0 +1,11 @@
+""" exercicio 01 - recebe três números como parâmetro (n1, n2, n3)
+e retorna a soma dos números """
+
+n1 = 10
+n2 = 20
+n3 = 30
+
+def somar(n1, n2, n3):
+    return n1 + n2 + n3
+
+print(somar(n1, n2, n3))
