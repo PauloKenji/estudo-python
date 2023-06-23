1c2c9aadefc9a5423581c22d3021d5e340574b59
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-23 14:25:49 -0300

    08: exercicio 02 - update pessoa

--- src/08-modulos-e-pacotes/exercícios/pessoa/pessoa.py
+++ src/08-modulos-e-pacotes/exercícios/pessoa/pessoa.py
@@ -1,11 +1,12 @@
 """ Pessoa """
 
 class Pessoa:
-    def __init__(self, cpf, nome, telefone, endereco):
-        self.cpf = cpf
+    def __init__(self, nome, sobrenome, cpf, telefone=None, enderecos=None):
         self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
         self.telefone = telefone
-        self.enderecos = [endereco]
+        self.enderecos = enderecos if enderecos is not None else []
 
     def add_endereco(self, endereco):
         self.enderecos.append(endereco)
@@ -15,4 +16,4 @@ class Pessoa:
             print(endereco)
 
     def __str__(self):
-        return f'Pessoa[cpf={self.cpf},nome={self.nome},telefone={self.telefone},enderecos={self.enderecos}]'
+        return f'Pessoa[cpf={self.cpf},nome={self.nome},sobrenome={self.sobrenome}, telefone={self.telefone},enderecos={self.enderecos}]'
