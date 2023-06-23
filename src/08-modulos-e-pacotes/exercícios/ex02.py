087f70e452b7fe6bd1f34703266cd97cf226f037
Paulo Kenji <paulomuneischi@gmail.com>
2023-06-23 13:39:31 -0300

    08: exercicio 02 - pessoa

--- src/08-modulos-e-pacotes/exercícios/ex02.py
+++ src/08-modulos-e-pacotes/exercícios/ex02.py
@@ -16,7 +16,22 @@ print(type(retangulo2), retangulo1.base, retangulo2.altura, retangulo2.calcular_
 
 # ============= Pessoas =============
 
+pessoa1 = Pessoa('Maria da Silva', 'maria@email.com')
+pessoa2 = Pessoa('João Santos', 'joao@email.com')
+pessoa3 = Pessoa('Pedro Santos','Pedro@email.com')
+#alterando o valor do atributo de classe na instancia
+# altera somente na instancia
+pessoa1.especie = 'Alienigena'
 
+#alterando o valor do atributo de classe na classe
+# altera em todas as instancias e na classe também
+Pessoa.especie = 'Alienigenas do Passado'
+
+print(pessoa1.nome, pessoa1.email, pessoa1.especie)
+print(pessoa2.nome, pessoa2.email, pessoa2.especie)
+print(pessoa3.nome, pessoa3.email, pessoa3.especie)
+
+print(Pessoa.especie)
 
 # ============= Alunos, Projetos e Participações =============	
 
---
+++ src/08-modulos-e-pacotes/exercícios/pessoa/pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa:
+
+    especie = 'Humana'
+
+    def __init__(self, nome, email):
+        self.nome = nome
+        self.email = email
